fb37d4fc68f7a777cdc5fd81958c84e1ee5d6e42
Priya <priya.mishra@osganalytics.com>
2021-05-30 10:54:55 +0530

    changes

--- src/main/java/com/amazon/sns/controller/SNSController.java
+++ src/main/java/com/amazon/sns/controller/SNSController.java
@@ -1,13 +1,13 @@
 package com.amazon.sns.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SNSController {
@@ -15,18 +15,8 @@ public class SNSController {
     static ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping(path = "/notification", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView index(@RequestBody(required = false) String snsSubscriptionRequestString) throws JsonProcessingException {
-        System.out.println("SNS Subscription Request String : " + snsSubscriptionRequestString);
-        ModelAndView modelAndView = new ModelAndView("index");
-        if (snsSubscriptionRequestString != null) {
-            snsSubscriptionRequestString = snsSubscriptionRequestString.replace("\n", "");
-            JSONObject jsonObject = new JSONObject(snsSubscriptionRequestString);
-            final String subscribeURL = jsonObject.optString("SubscribeURL");
-            modelAndView = new ModelAndView("notification");
-            modelAndView.addObject("subscribeURL", subscribeURL);
-        }
-        return modelAndView;
+    public void index(@RequestBody(required = false) String snsRequest, HttpServletRequest httpServletRequest,
+                      HttpServletResponse httpServletResponse) {
+        System.out.println("SNS Request String : " + snsRequest);
     }
-
-
 }
